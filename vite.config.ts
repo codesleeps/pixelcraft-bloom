34ff2e34b8f19784313910d86ea94f5ca2d94f43
codesleeps <codessleep43@gmail.com>
2025-12-13 12:58:43 +0000

    Configure GitHub Pages with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/pixelcraft-bloom/' : './',
+  base: '/',
   server: {
     host: "::",
     port: 5173,
